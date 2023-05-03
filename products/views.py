6311d0d7370276c6d59817ac9c44b99496bc554a
VictoriaT87 <victoriaemt@gmail.com>
2023-05-03 21:14:57 +0000

    Add starting product template, view, url

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from .models import Product
+from .models import Product, Brand, Category
 from .forms import ProductForm
+
+import random
 
 
 def all_products(request):
@@ -19,3 +21,20 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show individual product details
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    products = Product.objects.all()
+
+    template = 'products/product_detail.html'
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
