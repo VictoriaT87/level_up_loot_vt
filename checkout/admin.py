f08a74d3a465813acb0bbdc225a6573e78b188f6
VictoriaT87 <victoriaemt@gmail.com>
2023-05-07 18:33:04 +0000

    Add checkout forms.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,15 +13,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_bag',
-                       'stripe_pid')
+                       'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total', 'original_bag',
-              'stripe_pid')
+              'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
