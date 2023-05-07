cb53590a3a70503ff88ff57ffa26773ba3f8b055
VictoriaT87 <victoriaemt@gmail.com>
2023-05-07 10:32:57 +0000

    Add view/template for add to cart, update contexts.py, change to euros

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,7 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
 
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
