51a511a452dd3f78677543970ac2c883aa33cec0
VictoriaT87 <victoriaemt@gmail.com>
2023-05-10 11:13:40 +0000

    Add loading overlay, update checkout_success view

--- checkout/views.py
+++ checkout/views.py
@@ -98,19 +98,10 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach the user's profile to the order
-        order.user_profile = profile
-        order.save()
-
-
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
-    # delete cart when checkout done
     if 'cart' in request.session:
         del request.session['cart']
 
