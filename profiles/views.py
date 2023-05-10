8e33d14c285dc79cbef26d0d03571db7c476f99c
VictoriaT87 <victoriaemt@gmail.com>
2023-05-10 13:32:08 +0000

    Update profile view, url on nav-items, profile template to test

--- profiles/views.py
+++ profiles/views.py
@@ -12,22 +12,9 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
     template = 'profiles/profile.html'
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True
+        'profile': profile
     }
 
     return render(request, template, context)
