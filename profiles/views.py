d1f8a39bb89650cda8cedbe0c37e267342379252
VictoriaT87 <victoriaemt@gmail.com>
2023-05-10 14:07:38 +0000

    Add orderform, update view and profile template

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 from checkout.models import Order
 
@@ -12,9 +13,14 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
