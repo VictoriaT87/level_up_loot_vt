e981b5eaf43d8b7a7b5c1b254635610e8b93e198
VictoriaT87 <victoriaemt@gmail.com>
2023-05-10 16:02:31 +0000

    Add delete views, add links to product cards for CRUD

--- products/views.py
+++ products/views.py
@@ -148,3 +148,23 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """
+    Delete a product from the store
+    """
+
+    if not request.user.is_superuser:
+        messages.error(
+            request,
+            "Sorry, only store owners can do that.")
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product_title = product.title
+    product.delete()
+    messages.info(request, f'{product_title} deleted')
+
+    return redirect(reverse('products'))
