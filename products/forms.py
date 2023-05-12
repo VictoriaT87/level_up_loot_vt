a8145787c4acf7db1bcbb50e6f45f26c4ca07f4d
VictoriaT87 <victoriaemt@gmail.com>
2023-05-12 12:33:55 +0000

    Add custom field image widget for products

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category, Brand
 
 
@@ -8,7 +9,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
