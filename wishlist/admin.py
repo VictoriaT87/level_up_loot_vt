cc994a5f623ec2d21b674bf2b139c73998247bbb
VictoriaT87 <victoriaemt@gmail.com>
2023-05-14 20:27:49 +0000

    Add wishlist admin, update template

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Wishlist
 
 # Register your models here.
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'product',
+        'date_added',
+    )
+
+    ordering = ('user',)
+
+admin.site.register(Wishlist, WishlistAdmin)
