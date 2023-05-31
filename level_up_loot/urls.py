94d78798def30a2c7c591735c583eab8f01a4c17
VictoriaT87 <victoriaemt@gmail.com>
2023-05-31 13:04:14 +0000

    Add handler404 templae, url, view

--- level_up_loot/urls.py
+++ level_up_loot/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'level_up_loot.views.handler404'
