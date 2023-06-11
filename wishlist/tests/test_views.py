c4d89f233a2d68add8aad9450b21d117f2cec096
VictoriaT87 <victoriaemt@gmail.com>
2023-06-11 14:25:12 +0000

    Write tests for wishlist add view

--- wishlist/tests/test_views.py
+++ wishlist/tests/test_views.py
@@ -4,10 +4,11 @@ from django.contrib.messages import get_messages
 from django.urls import reverse
 from profiles.models import UserProfile
 from wishlist.models import Wishlist
-from wishlist.views import view_wishlist
+from products.models import Product
 
 
-class WishlistViewTest(TestCase):
+class ViewWishlistTest(TestCase):
+    # Test a user viewing the wishlist
     @classmethod
     def setUpClass(cls):
         """
@@ -42,9 +43,6 @@ class WishlistViewTest(TestCase):
 
 
     def test_unauthenticated_user_view_wishlist(self):
-        # User is none (not logged in/authorized)
-        user = None
-
         # Make a GET request to the wishlist view
         response = self.client.get(reverse('wishlist'))
 
@@ -58,3 +56,45 @@ class WishlistViewTest(TestCase):
         # https://stackoverflow.com/questions/2897609/how-can-i-unit-test-django-messages/14909727#14909727
         messages = [m.message for m in get_messages(response.wsgi_request)]
         self.assertIn('Sorry, you need to be logged in to add your Wishlist.', messages)
+
+
+class AddWishlistTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Create a user and a product for testing
+        """
+        super().setUpClass()
+        cls.client = Client()
+        cls.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        cls.product = Product.objects.create(
+            title="Test Product",
+            description="Test description",
+            price='9.99',
+        )
+
+
+    def test_authenticated_user_add_wishlist(self):
+        # Create a UserProfile if it doesn't exist
+        user_profile = UserProfile.objects.get_or_create(user=self.user)
+
+        # Log in the user
+        self.client.force_login(self.user)
+
+        # Add something to the wishlist
+        response = self.client.post(reverse('add_wishlist', args=[self.product.id]))
+
+        # Assert that the response status code is 302 (redirect)
+        self.assertEqual(response.status_code, 302)
+
+        # Assert that the user is redirected to the product detail page
+        self.assertRedirects(response, reverse('product_detail', args=[self.product.id]))
+
+        # Assert that the product is added to the wishlist
+        wishlist = Wishlist.objects.get(user=self.user)
+        self.assertIn(self.product, wishlist.products.all())
+
+        # Assert that the success message is displayed
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertIn(f'{self.product.title} has been added to your Wishlist!', messages)
