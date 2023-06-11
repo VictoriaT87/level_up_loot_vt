8ee346cdce5c0822152884fb9df0e7e6083e7f82
VictoriaT87 <victoriaemt@gmail.com>
2023-06-11 13:14:58 +0000

    Install coverage and pytest and write first tests

--- level_up_loot/settings.py
+++ level_up_loot/settings.py
@@ -127,17 +127,17 @@ WSGI_APPLICATION = 'level_up_loot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ wishlist/tests/test_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from products.models import Product
+from wishlist.models import Wishlist
+
+class WishlistModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a test User
+        user = User.objects.create(username='testuser')
+        # Create a test product
+        product = Product.objects.create(
+            title="Test Product",
+            description="Test description",
+            price='9.99',
+        )
+
+        # Create a test Wishlist
+        Wishlist.objects.create(user=user, date_added='2023-06-01')
+
+        # Add the product to the wishlist
+        wishlist = Wishlist.objects.get(user=user)
+        wishlist.products.add(product)
+
+    def test_string_representation(self):
+        # test the returned string
+        wishlist = Wishlist.objects.get(id=1)
+        expected_str = f"{wishlist.user.username}'s Wishlist"
+        self.assertEqual(str(wishlist), expected_str)
+
+    def test_products_many_to_many_relationship(self):
+        # test that the wishlist has a product in it
+        wishlist = Wishlist.objects.get(id=1)
+        products = wishlist.products.all()
+        self.assertEqual(len(products), 1)
+
+    def test_user_one_to_one_relationship(self):
+        # test that the user is not none and has the expected username
+        wishlist = Wishlist.objects.get(id=1)
+        user = wishlist.user
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, 'testuser')
+
+    def test_date_added_field(self):
+        wishlist = Wishlist.objects.get(id=1)
+        date_added = wishlist.date_added
+        self.assertIsNotNone(date_added)
