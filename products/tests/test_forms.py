5cc678e72da5692024b2362e8dd03b6da8998523
VictoriaT87 <victoriaemt@gmail.com>
2023-06-11 19:39:18 +0000

    Test product form

---
+++ products/tests/test_forms.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from products.forms import ProductForm
+from products.models import Category, Brand, Product
+
+class ProductFormTest(TestCase):
+    """
+    Test Product Form
+    """
+
+    def test_form_fields(self):
+        # Test form fields
+        form = ProductForm()
+        self.assertEqual(form.Meta.model, Product)
+        self.assertEqual(form.Meta.fields, '__all__')
+        self.assertEqual(form.Meta.exclude, ('created_on',))
+
+    def test_form_choices(self):
+        # Test form choices
+        category = Category.objects.create(name='Category')
+        brand = Brand.objects.create(name='Brand')
+
+        form = ProductForm()
+        self.assertEqual(
+            form.fields['category'].choices,
+            [(category.id, category.get_friendly_name())]
+        )
+        self.assertEqual(
+            form.fields['brand'].choices,
+            [(brand.id, brand.get_friendly_name())]
+        )
