7f9d25a24b34e2c33c268b6102e237d6e0f0d515
VictoriaT87 <victoriaemt@gmail.com>
2023-06-15 18:23:33 +0000

    Test Checkout View, Checkout Success View

--- checkout/tests/test_views.py
+++ checkout/tests/test_views.py
@@ -2,8 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.messages import get_messages
 from django.shortcuts import get_object_or_404
-from datetime import date, timedelta
-from checkout.models import Coupon
+from datetime import date
+from checkout.models import Coupon, Order
 
 
 class AddCouponViewTest(TestCase):
@@ -36,7 +36,6 @@ class AddCouponViewTest(TestCase):
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(str(messages[0]), f'Coupon code: test applied')
 
-    
     def test_add_wrong_coupon(self):
         # Test Add Wrong Coupon
 
@@ -68,7 +67,7 @@ class RemoveCouponViewTest(TestCase):
 
     def test_remove_coupon(self):
         # Test Remove Coupon
-        
+
         # Add the coupon ID to the session
         self.client.session['coupon_id'] = 1
 
@@ -81,3 +80,52 @@ class RemoveCouponViewTest(TestCase):
         # Check the message returned
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(str(messages[0]), "The coupon has been removed")
+
+
+class CheckoutViewTest(TestCase):
+    """ Test Checkout View """
+
+    def test_empty_cart(self):
+        # Test Checkout View with an Empty Cart
+
+        # Create empty cart
+        self.client.session['cart'] = {}
+
+        # Call the view
+        response = self.client.get(reverse('checkout'))
+
+        # Check the messages in the response
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(
+            str(messages[0]), "There's nothing in your cart at the moment")
+
+
+class CheckoutSuccessViewTest(TestCase):
+    """ Test Checkout Success View """
+
+    def setUp(self):
+        # Setup Client
+        self.client = Client()
+
+        # Setup Order
+        self.order = Order.objects.create(
+            order_number='12345',
+            email='test@test.com'
+        )
+
+    def test_checkout_success_view(self):
+        # Test Checkout Success View Template and Message
+
+        response = self.client.get(reverse('checkout_success', args=['12345']))
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the email message sent
+        messages = [str(message)
+                    for message in get_messages(response.wsgi_request)]
+        self.assertIn(
+            f'Order successfully processed! \
+        Your order number is {self.order.order_number}. A confirmation \
+        email will be sent to {self.order.email}.', messages)
+
+        # Assert that the correct template is used
+        self.assertTemplateUsed(response, 'checkout/checkout_success.html')
