f035e18ff8290b7f7296c4cef844505bd4515210
VictoriaT87 <victoriaemt@gmail.com>
2023-07-10 13:01:57 +0000

    Add error pages for 403 and 500

--- level_up_loot/views.py
+++ level_up_loot/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+def handler403(request, exception):
+    """ Error Handler 403 """
+    return render(request, "errors/403.html", status=403)
+
+def handler500(request, *args, **argv):
+    """ Error Handler 500 """
+    return render(request, "errors/500.html", status=500)
